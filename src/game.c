fd4fdfc434547ec6ea03ec6d44af0ec2fd98b921
Thierry <thierry.legot@gmail.com>
2026-03-04 21:47:16 +0100

    Etape 2 - Affichage du score

--- src/game.c
+++ src/game.c
@@ -24,13 +24,16 @@
 
 #include <mo5_defs.h>
 #include <mo5_sprite_bg.h>
+#include <mo5_font6.h>
 #include "game.h"
 #include "assets/player.h"
 #include "assets/bullet_player.h"
 
+#define DRAWING_BLOC_HIGH   6
 #define MAX_BULLETS_PLAYER  3
 #define PLAYER_SPEED_X      1
 #define BULLET_SPEED_Y      4
+#define PLAYER_MAX_LIFE     3
 
 typedef struct {
     MO5_Actor     actor;
@@ -49,7 +52,7 @@ static void game_init_player(void)
 
     player_actor.sprite  = &player_sprite;
     player_actor.pos.x   = (SCREEN_WIDTH_BYTES - SPRITE_PLAYER_WIDTH_BYTES) / 2;
-    player_actor.pos.y   = SCREEN_HEIGHT - SPRITE_PLAYER_HEIGHT - 2;
+    player_actor.pos.y   = SCREEN_HEIGHT - SPRITE_PLAYER_HEIGHT;
     player_actor.old_pos = player_actor.pos;
 
     for (i = 0; i < MAX_BULLETS_PLAYER; i++) {
@@ -82,7 +85,7 @@ static void game_update_bullets(void)
     for (i = 0; i < MAX_BULLETS_PLAYER; i++) {
         if (!bullets_player[i].active) continue;
 
-        if (bullets_player[i].actor.pos.y >= BULLET_SPEED_Y) {
+        if (bullets_player[i].actor.pos.y >= DRAWING_BLOC_HIGH + BULLET_SPEED_Y) {
             mo5_actor_move_bg(&bullets_player[i].actor,
                               bullets_player[i].actor.pos.x,
                               bullets_player[i].actor.pos.y - BULLET_SPEED_Y);
@@ -95,11 +98,43 @@ static void game_update_bullets(void)
     }
 }
 
+static void display_score(unsigned char score) {
+    // sprintf fait planter le programme.
+    // char buf[12];
+    // sprintf(buf, "SCORE: %03u", score);
+    // mo5_font6_puts(0, 0, buf, C_BLUE);
+    char buf[4];
+    buf[0] = '0' + (score / 100);
+    buf[1] = '0' + (score % 100 / 10);
+    buf[2] = '0' + (score % 10);
+    buf[3] = '\0';
+
+    mo5_font6_puts(0, 0, "SCORE:", C_BLUE);
+    mo5_font6_puts(6, 0, buf, C_BLUE);
+}
+
+static void display_live(unsigned char live) {
+    // sprintf fait planter le programme.
+    // char buf[7];
+    // sprintf(buf, "VIE: %u", live);
+    // mo5_font6_puts(35, 0, buf, C_BLUE);
+    char buf[2];
+    buf[0] = '0' + live;
+    buf[1] = '\0';
+    mo5_font6_puts(35, 0, "VIE:", C_BLUE);
+    mo5_font6_puts(39, 0, buf, C_BLUE);
+}
+
 void game_loop(void)
 {
     const unsigned char max_x = SCREEN_WIDTH_BYTES - SPRITE_PLAYER_WIDTH_BYTES;
+    unsigned char score = 0;
+    unsigned char live = PLAYER_MAX_LIFE;
     game_init_player();
     mo5_actor_draw_bg(&player_actor);
+
+    display_score(score);
+    display_live(live);
 
     unsigned char input;
     unsigned char new_x = player_actor.pos.x;
